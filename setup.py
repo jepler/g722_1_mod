1cf93ca32d3dabc11b3f28d1bbfccb8462412e4e
Jeff Epler <jepler@gmail.com>
2023-07-03 12:32:38 -0500

    bump version number for release

--- setup.py
+++ setup.py
@@ -3,16 +3,12 @@ import glob
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
 # * You can set include_pybind11=false to add the include directory yourself,
 #   say from a submodule.
-#
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/g722_1_mod/pull/53)
 
 ext_modules = [
     Pybind11Extension("g722_1_mod",
