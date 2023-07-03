486e40de88c6c9d39e09b2d918580f744a70b1db
Jeff Epler <jepler@gmail.com>
2023-07-03 11:53:13 -0500

    simplify typedefs, it's 2023

--- src/common/typedef.h
+++ src/common/typedef.h
@@ -16,40 +16,13 @@
 #ifndef TYPEDEF_H
 #define TYPEDEF_H "$Id $"
 
-#include <limits.h>
-
-#if defined(__BORLANDC__) || defined(__WATCOMC__) || defined(_MSC_VER) || defined(__ZTC__) || defined(__CYGWIN__)
-typedef signed char Word8;
-typedef short Word16;
-typedef long Word32;
-typedef int Flag;
-
-#elif defined(__sun)
-typedef signed char Word8;
-typedef short Word16;
-typedef long Word32;
-typedef int Flag;
-
-#elif defined(__unix__) || defined(__unix) || defined (__IPHONE_2_0)
-typedef signed char Word8;
-typedef short Word16;
-typedef int Word32;
-typedef int Flag;
-
-#endif
-
-/* define 16 bit unsigned types for G.722.1 */
-#if INT_MAX == 32767
-typedef unsigned int UWord16;
-#elif SHRT_MAX == 32767
-typedef unsigned short UWord16;
-#endif
-
-/* define 32 bit unsigned types for G.722.1 */
-#if INT_MAX == 2147483647L
-typedef unsigned int UWord32;
-#elif LONG_MAX == 2147483647L
-typedef unsigned long UWord32;
-#endif
+#include <stdint.h>
+#include <stdbool.h>
+typedef int8_t Word8;
+typedef int16_t Word16;
+typedef uint16_t UWord16;
+typedef int32_t Word32;
+typedef uint32_t UWord32;
+typedef bool Flag;
 
 #endif /* TYPEDEF_H */
